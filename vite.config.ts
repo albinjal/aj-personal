5f53ce62f5df84104919a4dde98b9d2447f46791
Albin Jaldevik <albja664@student.liu.se>
2025-06-01 23:13:56 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // Set for custom domain jaldevik.com
+  base: "/aj-personal/",
   server: {
     host: "::",
     port: 8080,
